438297f9f750bfe897d444555075d3b96c162f74
akhtarsa <engineer.ashar@gmail.com>
2014-03-04 22:39:44 +0000

    First
    Test

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html >
+<head>
+    <meta  charset="utf-8">
+    <title>AD Clothing</title>
+
+    <!-- 
+    <link rel="stylesheet" href="assets/css/jquery-ui-1.10.2.css" > -->
+
+</head>
+
+<body >
+    <style>
+        body 
+        { 
+            background:#F03914;
+            background-image: url(ad2.png); 
+            background-position:50% 50%;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            
+        }
+        #footer{
+        position: fixed;
+        margin-top:-150px;
+        margin-left: auto;
+         bottom:0px;
+         left:0px;
+         width:100%;
+
+   
+        }
+    </style>
+    <div id="topdiv" >
+       <h1 style="text-align: center;"> AD Clothing </h2>
+    </div>
+
+    <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>-->
+
+    <!--<script src="assets/js/jquery-1.9.1.js"></script>
+    <script src="assets/js/jquery-ui-1.10.2.js"></script> -->
+    <h3 style="text-align: center;">
+       We will be right back soon!!
+    </h4>
+    </div>
+<div id="footer"> 
+    <center>
+<form action='http://cp.hosting.123-reg.co.uk/list.cgi' method='post'>
+<input type='hidden' name='list' value='customers'/>
+<input type='hidden' name='redirect-success' value='http://www.adclothing.com/?action=done'/>
+<b>Our Mailing List</b><br/>
+<input type='text' name='email'/><br/>
+<input class='radio' type='radio' name='action' value='add' checked='checked'/>Subscribe
+<input class='radio' type='radio' name='action' value='delete'/>Unsubscribe<br/>
+<input type='submit' value='Do it Now!'/>
+</form>
+</center>
+</div>
+
+</body>
+</html>
+
